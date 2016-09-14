f162b77251ab84ed56bc1ed02b74f183c5479a98
Jeff Horton <jeff@goblinoid.co.uk>
2016-09-14 23:36:43 +0100

    Design Sidebar * Sidebar template * Example magery data

--- public/scripts/magery.js
+++ public/scripts/magery.js
@@ -2,6 +2,7 @@ import { actions } from './actions.js'
 
 export const mapState = state => ({
     modeHandler: getModeHandler(state.mode)
+    sidebar:     buildSidebar(state)
 })
 
 function getModeHandler(mode) {
@@ -11,3 +12,33 @@ function getModeHandler(mode) {
         case actions.MODE_ACCOUNT:     return 'account';
     }
 }
+
+function buildSidebar = state => ({
+    filters: [
+        {
+            id: "events",
+            filterType: "filterGroup"
+            label: "Events",
+            expanded: false,
+            filters: [
+                {
+                    id: "event-1",
+                    label: "Derby - Full Warbands",
+                    selected: false
+                },
+                {
+                    id: "event-2",
+                    label: "Derby - Wizard Groups",
+                    selected: false
+                }
+            ]
+        },
+        {
+            id: "my-rosters",
+            filterType: "filter",
+            label: "My Rosters",
+            selected: true
+        }
+    ],
+    admin: true
+})
