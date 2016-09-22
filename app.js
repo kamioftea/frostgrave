e70f8713ef14a90aff2ff3a35a75a6843693c7d5
Jeff Horton <jeff@goblinoid.co.uk>
2016-09-22 23:25:59 +0100

    Production changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('./passport.js');
 var session = require('express-session');
+var MongoStore = require('express-session-mongo');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,7 +26,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //TODO: load secret from config
-app.use(session({ secret: 'frostgrave', resave: false, saveUninitialized: false }));
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'frostgrave',
+  resave: false, saveUninitialized: false,
+  store: new MongoStore({db: 'frostgrave-session'})
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -35,6 +41,7 @@ app.use(require('node-sass-middleware')({
   sourceMap: true,
   includePaths: [path.join(__dirname, 'node_modules', 'foundation-sites', 'scss')]
 }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -43,9 +50,7 @@ app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.render('error/not-found', {title: 'Not Found - Frostgrave Roster Management', url: req.baseUrl + req.url})
 });
 
 // error handlers
@@ -55,7 +60,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error/error', {
       message: err.message,
       error: err
     });
@@ -66,7 +71,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render('error/error', {
     message: err.message,
     error: {}
   });
